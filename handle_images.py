59caa946c06f2c982efc4d35474d62d4b04be924
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-05-08 12:05:21 -0300

    some fixes

--- handle_images.py
+++ handle_images.py
@@ -2,7 +2,7 @@ import os, PIL
 from PIL import Image
 from os import listdir
 
-PATH = "data/"
+PATH = "dataset/"
 ROOT = os.getcwd()
 
 def handle_prictures(directories, path):
@@ -11,24 +11,30 @@ def handle_prictures(directories, path):
     
     directory = os.path.join(path,d)   
     d = "{}/{}".format(ROOT, directory)
-    
+    print ("[INFO] directory: {}".format(d))
     os.chdir(d)
 
     for img in listdir(os.getcwd()):
       
-      print (img)
+      print ("[INFO] image: {}".format(img))
       image = Image.open(img)
       newImage = image.resize((256,256), Image.ANTIALIAS)
       newImage.save(img, optimize=True, quality=95)
 
       picture = Image.open(img)
-      picture.rotate(45, expand=True).save('rotated{}_{}'.format('45',img), optimize=True, quality=95)
+      picture.rotate(45, expand=True).save('rotated{}_{}'.format('45',img))
       picture.rotate(90, expand=True).save('rotated{}_{}'.format('90',img), optimize=True, quality=95)
-      picture.rotate(135, expand=True).save('rotated{}_{}'.format('135',img), optimize=True, quality=95)
+      picture.rotate(135, expand=True).save('rotated{}_{}'.format('135',img))
       picture.rotate(180, expand=True).save('rotated{}_{}'.format('180',img), optimize=True, quality=95)
-      picture.rotate(225, expand=True).save('rotated{}_{}'.format('225',img), optimize=True, quality=95)
+      picture.rotate(225, expand=True).save('rotated{}_{}'.format('225',img))
       picture.rotate(270, expand=True).save('rotated{}_{}'.format('270',img), optimize=True, quality=95)
-      picture.rotate(315, expand=True).save('rotated{}_{}'.format('315',img), optimize=True, quality=95)  
+      picture.rotate(315, expand=True).save('rotated{}_{}'.format('315',img))
+
+      for angle in [45,135,225,315]:
+        name = 'rotated{}_{}'.format(angle, img)
+        image = Image.open(name)
+        new_image = image.resize((256,256), Image.ANTIALIAS)
+        new_image.save(name, optimize=True, quality=95)  
    
 def main():
 
